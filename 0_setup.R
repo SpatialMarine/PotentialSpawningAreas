592a4a3b3b81bd783b0818afee857ee1ded1a23b
davidruizgarci <david.ruiz-garcia@uv.es>
2024-06-29 13:29:56 +0200

    commit

---
+++ 0_setup.R
@@ -0,0 +1,28 @@
+#--------------------------------------------------------------------------------
+# setup.R         Setup project
+#--------------------------------------------------------------------------------
+
+# 1. set computer
+cpu <- "david"  
+
+# Set main data paths
+if(cpu == "david") main_dir <- "C:/Users/david/OneDrive/Escritorio/Huevos"
+setwd(main_dir)
+
+# 2. Create data paths
+input_data <- paste(main_dir, "input", sep="/")
+if (!dir.exists(input_data)) dir.create(input_data, recursive = TRUE)
+
+temp_data <- paste(main_dir, "temp", sep="/")
+if (!dir.exists(temp_data)) dir.create(temp_data, recursive = TRUE)
+
+output_data <- paste(main_dir, "output", sep="/")
+if (!dir.exists(output_data)) dir.create(output_data, recursive = TRUE)
+
+# 3. load CEMEMS username / password based on cpu
+
+if(cpu == "david") f <- "C:/Users/david/OneDrive/Escritorio/chondrichthyan_habitat/user.txt"
+username <- paste(readLines(f, warn = FALSE), collapse = "")
+
+if(cpu == "david") f <- "C:/Users/david/OneDrive/Escritorio/chondrichthyan_habitat/psw.txt"
+password <- paste(readLines(f, warn = FALSE), collapse = "")
